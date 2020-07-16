5f50144d284b23702aad2d8f4bd290496da2596a
zhangleilei <zhangleilei@lecarlink.com>
2020-07-16 10:31:42 +0800

    ceshi

---
+++ .idea/dataSources/f73528c0-4ec5-4307-95be-8f9baaa1662f/storage_v2/_staging_/schema/LCOA.iZEjAA/package/PKG_ZL_ROLE.0MWI5A/body/PKG_ZL_ROLE.0MWI5A.sql
@@ -0,0 +1,11 @@
+create package body PKG_ZL_ROLE is
+  --新增部门阿阿斯顿阿瓦达群无多强无敌强无敌强无敌强无敌强无敌强无敌
+  function CREATEROLE(DataInfo        in varchar2, --新增的部门名称^父部门ID^LEVEL
+                                        ErrMsg          out varchar2)
+    return number is
+  begin
+  end; 
+
+end PKG_ZL_ROLE;
+/
+
