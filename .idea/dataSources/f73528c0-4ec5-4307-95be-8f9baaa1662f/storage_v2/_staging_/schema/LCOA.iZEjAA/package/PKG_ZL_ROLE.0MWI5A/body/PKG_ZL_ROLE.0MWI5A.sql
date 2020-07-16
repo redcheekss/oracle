f12368b2aadea1edcbdc98755d7b54a5a8a1de4b
zhangleilei <zhangleilei@lecarlink.com>
2020-07-16 14:09:27 +0800

    ceshi

--- .idea/dataSources/f73528c0-4ec5-4307-95be-8f9baaa1662f/storage_v2/_staging_/schema/LCOA.iZEjAA/package/PKG_ZL_ROLE.0MWI5A/body/PKG_ZL_ROLE.0MWI5A.sql
+++ .idea/dataSources/f73528c0-4ec5-4307-95be-8f9baaa1662f/storage_v2/_staging_/schema/LCOA.iZEjAA/package/PKG_ZL_ROLE.0MWI5A/body/PKG_ZL_ROLE.0MWI5A.sql
@@ -1,6 +1,8 @@
 create package body PKG_ZL_ROLE is
   --新增部门阿阿斯顿阿瓦达群无多强无敌强无敌强无敌强无敌强无敌强无敌asdqwdqwdwqdqwdqwdqwdqwdqwdqwdqwdqwdqwd
 --       asdwqdqwdqwdwqdqwdqwdqwdasd
+--       sadqwdwqdwqd
+--       asdqwd
   function CREATEROLE(DataInfo        in varchar2, --新增的部门名称^父部门ID^LEVEL
                                         ErrMsg          out varchar2)
     return number is
